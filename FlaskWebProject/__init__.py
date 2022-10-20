d4d33e3a8f05dd60871ba70c238fdf5df3853087
mohaned0 <hajeer-11@hotmail.com>
2022-10-21 01:48:22 +0300

    new a messag1e

--- FlaskWebProject/__init__.py
+++ FlaskWebProject/__init__.py
@@ -7,6 +7,7 @@ from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_session import Session
+from flask.logging import create_logger
 
 app = Flask(__name__)
 app.config.from_object(Config)
